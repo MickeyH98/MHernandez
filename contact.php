505fed6314944086b0ff1ae44e7c57944e90062f
Miguel Hernandez <miguelhernandez@MacBook-Pro.local>
2017-10-18 13:27:05 -0400

    added email functionality

--- contact.php
+++ contact.php
@@ -1,3 +1,79 @@
+<?php
+
+if(!empty($_POST["submit"])){ //when form submitted
+  var_dump($_POST);
+
+  //create shorthand variables
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $company = $_POST["company"];
+  $inquiry = $_POST["commentquestion"];
+  $errorMessage = []; //empty array for errors to be pushed into
+
+  //validate
+  //regex for validation
+  if(preg_match("/^[a-zA-Z ]+$/", $name) && //only letters
+  preg_match("/\S+@\S+\.\S+/", $email) && //string@string format
+  preg_match("/^[a-zA-Z ]+$/", $company) && //only letters
+  preg_match("/^[a-zA-Z0-9 '.,-]+$/", $inquiry)){ //only letters + numbers
+
+    //if validates, build email
+
+    //recipient
+    $to = 'mickeyh1998@gmail.com';
+
+    //subject
+    $subject = 'Portfolio Inquiry from ' . $name;
+
+    //build message
+    $message = '
+      Portfolio Inquiry
+
+      Name: ' . $name . '
+      Email: ' . $email . '
+      Company: ' . $company . '
+      Inquiry: ' . $inquiry;
+
+    //send email
+    mail($to, $subject, $message);
+
+    //create success message
+    $successMessage = "<p class='successMessage'>Thanks ". $name . ", your email has been sent and I'll get back to you soon!</p>";
+
+    //reset fields
+    $name = "";
+    $email = "";
+    $company = "";
+    $inquiry = "";
+
+    $success = true;
+
+  }
+  else { //if not valid, check which fields are incorrect
+    $success = false;
+    if(!preg_match("/^[a-zA-Z ]+$/", $name)){
+      array_push($errorMessage, "<p class='errorMessage'>Please enter the correct format name</p>"); //add error to errors array
+    }
+
+    if(!preg_match("/\S+@\S+\.\S+/", $email)){
+      array_push($errorMessage, "<p class='errorMessage'>Please enter the correct format email</p>");
+    }
+
+    if(!preg_match("/^[a-zA-Z ]+$/", $company)){
+      array_push($errorMessage, "<p class='errorMessage'>Please enter the correct format company</p>");
+    }
+
+    if(!preg_match("/^[a-zA-Z0-9 '.,-]+$/", $inquiry)){
+      array_push($errorMessage, "<p class='errorMessage'>Please enter the correct format inquiry</p>");
+    }
+
+    // display errors on page by echoing into html
+  }
+
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -5,9 +81,6 @@
 
     <?php require "lib/inc/headData.php"; ?>
 
-    <script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE="
-    crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
   </head>
   <body>
 
@@ -21,30 +94,54 @@
         </div>
         <div id="contactForm">
           <h3>Contact Form</h3>
-          <form id="formyForm" action="formProcess.php" method="post">
+
+          <div class="errorMessageWrapper">
+          <?php
+          if(!empty($errorMessage)){
+            foreach($errorMessage as $error){
+              echo $error;
+            }
+
+            echo "<button id='errorButton'>Okay</button>";
+          }
+          ?>
+          </div>
+
+          <div class="successMessageWrapper">
+            <?php
+            if($success){
+              echo $successMessage;
+              echo "<button id='successButton'>Okay</button>";
+            }
+            ?>
+          </div>
+
+          <form id="formyForm" action="contact.php" method="post">
 
             <div class="fieldWrapper">
               <label for="name">Name:</label>
-              <input type="text" name="name" id="name" required>
+              <input type="text" name="name" id="name" value="<?= $name ?>"required>
             </div>
 
             <div class="fieldWrapper">
               <label for="email">Email:</label>
-              <input type="email" name="email" id="email" required>
+              <input type="text" name="email" id="email" value="<?= $email ?>"required>
             </div>
 
             <div class="fieldWrapper">
               <label for="company">Company:</label>
-              <input type="text" name="company" id="company" required>
+              <input type="text" name="company" id="company" value="<?= $company ?>"required>
             </div>
 
             <div class="fieldWrapper">
               <label for="commentquestion">Inquiry:</label>
-              <input type="text" name="commentquestion" id="commentquestion" required>
+              <input type="text" name="commentquestion" id="commentquestion" value="<?= $inquiry ?>"required>
             </div>
 
             <input type="submit" value="Submit" name="submit" id="submitFormButton">
+
           </form>
+
         </div>
       </div>
     </div>
@@ -52,6 +149,7 @@
     <?php require "lib/inc/footer.php"; ?>
   </body>
 </html>
+
 
 <!--
 * Contact page - contact form with Name, Email, Phone, Company, Comments/Questions. You may add your phone and/or mailing address if you like. To submit the form, name, phone and email are required. Use JavaScript to make sure those fields are filled in and do not submit the form if they are not. Provide error message to user instead.
